83515d8892fbcfea67f665fc2b6aeb6c84df0671
matteopolak <matteopolak@hotmail.com>
2023-02-23 14:11:44 -0500

    chore: remove build folder

--- build/util/math.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.stdev = void 0;
-function stdev(array) {
-    const sum = array.reduce((a, b) => a + b, 0);
-    const inner = array.reduce((a, b) => a + (sum - b) ** 2, 0) / array.length;
-    return Math.sqrt(inner);
-}
-exports.stdev = stdev;
