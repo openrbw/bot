b8867c1d41ce44b62bd9c4d2bea397af73ba3c40
matteopolak <matteopolak@hotmail.com>
2023-04-21 12:38:50 -0400

    fix: /profile, add autocomplete for mode

--- src/commands/profile.ts
+++ src/commands/profile.ts
@@ -5,10 +5,11 @@ import {
 	CommandOptions,
 	CommandSource,
 	embed,
+	EventHandler,
 } from '@matteopolak/framecord';
 import { Mode } from '@prisma/client';
 import { prisma } from 'database';
-import { User } from 'discord.js';
+import { Events, Interaction, User } from 'discord.js';
 
 export default class ProfileCommand extends Command {
 	constructor(options: CommandOptions) {
@@ -17,29 +18,63 @@ export default class ProfileCommand extends Command {
 		this.description = 'Shows the profile of a player.';
 		this.arguments.push(
 			new Argument({
-				type: ArgumentType.User,
-				name: 'player',
-				description: 'The player whose profile you want to view',
-				default: s => s.user,
-				required: false,
-			}),
-			new Argument({
 				type: ArgumentType.String,
 				name: 'mode',
 				description: 'The mode of the profile to view',
-				required: false,
-				mapper: n => prisma.mode.findFirst({
+				required: true,
+				mapper: async n => await prisma.mode.findFirst({
 					where: {
 						nameLower: n.toLowerCase(),
 					},
 				}),
 				filter: m => m !== null,
 				error: 'Invalid mode provided.',
+				autocomplete: true,
+			}),
+			new Argument({
+				type: ArgumentType.User,
+				name: 'player',
+				description: 'The player whose profile you want to view',
+				default: s => s.user,
+				required: false,
 			})
 		);
 	}
 
-	public async run(source: CommandSource, user: User, mode: Mode) {
+	@EventHandler()
+	public async [Events.InteractionCreate](interaction: Interaction) {
+		if (!interaction.isAutocomplete()) return;
+		if (!this.is(interaction)) return;
+
+		const name = interaction.options.getString('mode', true).toLowerCase();
+		const modes = await prisma.mode.findMany({
+			where: {
+				nameLower: {
+					contains: name,
+				},
+			},
+			select: {
+				name: true,
+			},
+			take: 25,
+		});
+
+		if (modes.length) return interaction.respond(modes.map(m => ({ name: m.name, value: m.name })));
+
+		const defaultModes = await prisma.mode.findMany({
+			orderBy: {
+				name: 'asc',
+			},
+			select: {
+				name: true,
+			},
+			take: 25,
+		});
+
+		return interaction.respond(defaultModes.map(m => ({ name: m.name, value: m.name })));
+	}
+
+	public async run(source: CommandSource, mode: Mode, user: User = source.user) {
 		const profile = await prisma.profile.findFirst({
 			where: {
 				user: {
